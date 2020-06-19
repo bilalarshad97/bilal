a25a40128510d03663fd51800ea222f132c45d01
ASIF <ASIF@DESKTOP-1EM2CV9>
2020-06-19 15:36:41 +0500

    project

---
+++ src/main/resources/static/precache-manifest.d81ed9c7312d4258219bcfd3a4d22a79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37daa422dc0b5860a10340160d0f4501",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7f6b50bbb514bb1e870",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d07c2f1cc95c16353108",
+    "url": "/static/css/main.00d4352e.chunk.css"
+  },
+  {
+    "revision": "d7f6b50bbb514bb1e870",
+    "url": "/static/js/2.36ba4cfc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.36ba4cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07c2f1cc95c16353108",
+    "url": "/static/js/main.693425f9.chunk.js"
+  },
+  {
+    "revision": "3b98f9f1af4818cebb30",
+    "url": "/static/js/runtime-main.ca27617f.js"
+  }
+]);
